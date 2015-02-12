9b940f542aebf17deefb86710762c9280dcd6fda
Clark Feusier <cfeusier@gmail.com>
2015-02-12 12:29:09 -0800

    Move jotz into renderer/ and encapsulate

--- src/renderer/main.js
+++
@@ -1,6 +0,0 @@
-//Sets up React var globally
-var React = require('react');
-
-var sideMenu = require('./build/components/sideMenu.js');
-var underMenu = require('./build/components/underMenu.js');
-var noteView = require('./build/components/noteView.js');

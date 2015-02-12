1b8c212a7b5ccef53eabfc868348ee932ccf77dd
minocys <harich.leung@gmail.com>
2015-02-12 15:28:28 -0500

    Refactored handleMakeNote

---
+++ src/renderer/dispatcher/JotzDispatcher.js
@@ -0,0 +1,12 @@
+var Dispatcher = require('Flux').Dispatcher;
+
+var JotzDispatcher = new Dispatcher({
+  handleMakeNote = function(action){
+    var payload = {
+      action: action
+    };
+    this.dispatch(payload);
+  }
+});
+
+module.exports = JotzDispatcher;

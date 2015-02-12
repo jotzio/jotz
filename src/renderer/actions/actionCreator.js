9e56f8b46322df77a0f01b013388e3ae5dea5de8
minocys <harich.leung@gmail.com>
2015-02-12 17:17:24 -0500

    Added comment for example

--- src/renderer/actions/actionCreator.js
+++ src/renderer/actions/actionCreator.js
@@ -1,5 +1,7 @@
 var JotzDispatcher = require('../dispatcher/JotzDispatcher');
 
+
+//Example action called in noteView.js, line 5
 var actionCreator = {
   makeNote: function(content){
     JotzDispatcher.handleMakeNote({

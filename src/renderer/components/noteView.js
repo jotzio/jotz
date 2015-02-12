f412d396c1f04a2602c59184c369332a5bce52b1
minocys <harich.leung@gmail.com>
2015-02-12 03:24:44 -0500

    added noteviewmenu skeleton

--- src/renderer/components/noteView.js
+++ src/renderer/components/noteView.js
@@ -1,10 +1,21 @@
 var placeholder = 'Alas leggo my "leggo my eggo" legless lego legolas and leave illegible legalese legacies with legerity, lege of allegorist lasses';
 
+var NoteViewMenu = React.createClass({
+  render: function(){
+    return(
+      <h3>here|are|some|menu|items</h3>
+    );
+  }
+});
+
 var NoteView = React.createClass({
   render: function(){
     var classes = 'note ';
     return (
-      <div className={classes}> {this.props.note} </div>
+      <div className={classes}>
+        <NoteViewMenu />
+        {this.props.note}
+      </div>
     );
   }
 });

3a3ae8ee611b2fb4a450b0a94674cfbfb48b8f03
minocys <harich.leung@gmail.com>
2015-02-12 16:37:47 -0500

    Added dispatcher example

--- src/renderer/stores/Notebook.js
+++ src/renderer/stores/Notebook.js
@@ -1,6 +1,20 @@
+var JotzDispatcher = require('../dispatcher/JotzDispatcher');
+
 var Notebook = Backbone.collection.extend({
   initialize: function(){
+    this.dispatchToekn = JotzDispatcher.register(this.dispatchCallback);
+  },
 
+  //not sure if switch is best practice, just saw it in a few examples
+  dispatchCallback: function(payload){
+    switch(payload){
+      case 'make-notebook':
+        //do something to collection
+        break;
+      default:
+        break;
+    }
   }
-
 });
+
+module.exports = Notebook;

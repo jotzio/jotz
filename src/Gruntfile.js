e80b0d1fe9d7a19a6bf9985cd49df3ba7d76693f
Omar Alvarez <omar.alvarez2006@gmail.com>
2015-02-12 01:25:22 +0000

    Add new grunt task to work with browserify and reactify. It builds the files into a build.js file in the build directory.

--- src/Gruntfile.js
+++ src/Gruntfile.js
@@ -1,3 +1,6 @@
+
+var fs = require('fs');
+
 module.exports = function(grunt) {
   grunt.initConfig({
     'download-atom-shell': {
@@ -5,15 +8,51 @@ module.exports = function(grunt) {
       outputDir: '../atom_shell'
     },
 
-    react: {
-      single_file_output: {
+    // pkg: grunt.file.readJSON('package.json'),
+
+    env: {
+      build: {
+        NODE_ENV: 'production'
+      }
+    },
+
+    browserify: {
+      dev: {
+        options: {
+          debug: true,
+          transform: ['reactify']
+        },
         files: {
-          '../build/components/sideMenu.js': 'renderer/components/sideMenu.jsx'
+          '../build/build.js': 'renderer/components/**/*.jsx'
         }
+      },
+      build: {
+        options: {
+          debug: false,
+          transform: ['reactify']
+        },
+        files: {
+          '../build/build.js': 'renderer/main.js'
+        }
+      }
+    },
+
+    watch: {
+      browserify: {
+        files: ['renderer/**/*.jsx'],
+        tasks: ['browserify:dev']
+      },
+      options: {
+        nospawn: true
       }
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-env');
   grunt.loadNpmTasks('grunt-download-atom-shell');
-  grunt.loadNpmTasks('grunt-react');
+  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('build', ['env:build', 'browserify:build']);
+
 };

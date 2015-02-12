a731d9f8b7d080af82ba366a496942c3e5bd38b0
minocys <harich.leung@gmail.com>
2015-02-12 03:11:46 -0500

    added undermenu skeleton

---
+++ src/renderer/components/underMenu.js
@@ -0,0 +1,24 @@
+var SearchBar = React.createClass({
+  render: function(){
+    var classes = 'searchbar ';
+    return (
+      <input type='text' className={classes}></input>
+    );
+  }
+});
+
+var UnderMenu = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>HelloHello</h1>
+        <SearchBar />
+      </div>
+    );
+  }
+});
+
+React.render(
+  <UnderMenu />,
+  document.getElementById('undermenu')
+);

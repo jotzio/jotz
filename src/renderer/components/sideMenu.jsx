7616e3fdeb62207185270e488b8f8d205b1dc705
minocys <harich.leung@gmail.com>
2015-02-11 20:48:00 -0500

    react.js sidemenu template done

--- src/renderer/components/sideMenu.jsx
+++ src/renderer/components/sideMenu.jsx
@@ -1,11 +1,50 @@
 var React = require('react');
 
-var SideMenu = React.createClass({
-  render: function() {
+var MenuItems = React.createClass({
+  render: function(){
     return (
-      <h1>Hello Side Menu</h1>
+      <div>
+        <button>Notebooks</button>
+        <button>Notes</button>
+      </div>
     );
   }
 });
 
-React.render(<SideMenu />, document.getElementById('sidemenu'));
+var Settings = React.createClass({
+  render: function(){
+    var style = {
+      width: '60px',
+      height: '60px',
+      border: '1px solid black'
+    };
+    return (
+      <div style={style}>settings</div>
+    );
+  }
+});
+
+var SideMenu = React.createClass({
+  render: function(){
+    var style = {
+      width: '15%',
+      height: '100vh',
+      border: '1px solid red',
+      overflow: 'hidden'
+    };
+    return (
+      <div style={style}>
+        <MenuItems/>
+        <Settings/>
+      </div>
+    );
+  }
+});
+
+React.render(
+  <SideMenu />,
+  document.getElementById('sidemenu')
+);
+
+
+

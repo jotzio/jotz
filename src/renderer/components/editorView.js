0937c7f2491ac9ef1f41f362c0ef78549490ff77
minocys <harich.leung@gmail.com>
2015-02-13 15:09:25 -0500

    Add editorView

---
+++ src/renderer/components/editorView.js
@@ -0,0 +1,17 @@
+var React = require('react');
+var Ace = require('../../lib/ace/ace');
+
+var EditorView = React.createClass({
+  componentDidMount: function() {
+    var editor = Ace.edit('editor');
+    editor.setTheme('ace/theme/monokai');
+    editor.getSession().setMode('ace/mode/javascript');
+  },
+  render: function() {
+    return (
+      <div id='editor'></div>
+    );
+  }
+});
+
+module.exports = EditorView;

75619b97702b55526082980138520e2d09b7d1ed
Omar Alvarez <omar.alvarez2006@gmail.com>
2015-02-13 06:25:14 +0000

    Add intitial jasmine setup

---
+++ src/spec/component/noteViewSpec.js
@@ -0,0 +1,20 @@
+require('node-jsx').install();
+var React = require('react/addons');
+var TestUtils = React.addons.TestUtils;
+var jsdom = require('jsdom');
+
+describe('NoteView', function() {
+  var NoteView = require('../../renderer/components/noteView');
+
+  beforeEach(function() {
+    document = jsdom.jsdom();
+    window = document.parentWindow;
+    component = TestUtils.renderIntoDocument( React.createElement(NoteView, null));
+  });
+
+  it("renders", function() {
+    var foundComponent = TestUtils.isCompositeComponentWithType(component, NoteView);
+
+    expect(foundComponent).toBeTruthy();
+  });
+});

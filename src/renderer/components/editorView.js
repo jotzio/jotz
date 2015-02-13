8b9b38ab7cd1881f233ea1cbecf329a2db71ab56
minocys <harich.leung@gmail.com>
2015-02-13 15:19:51 -0500

    Comment out code for rebasing

--- src/renderer/components/editorView.js
+++ src/renderer/components/editorView.js
@@ -1,17 +1,17 @@
-var React = require('react');
-var Ace = require('../../lib/ace/ace');
-
-var EditorView = React.createClass({
-  componentDidMount: function() {
-    var editor = Ace.edit('editor');
-    editor.setTheme('ace/theme/monokai');
-    editor.getSession().setMode('ace/mode/javascript');
-  },
-  render: function() {
-    return (
-      <div id='editor'></div>
-    );
-  }
-});
-
-module.exports = EditorView;
+//var React = require('react');
+//var Ace = require('../../lib/ace/ace');
+//
+//var EditorView = React.createClass({
+//  componentDidMount: function() {
+//    var editor = Ace.edit('editor');
+//    editor.setTheme('ace/theme/monokai');
+//    editor.getSession().setMode('ace/mode/javascript');
+//  },
+//  render: function() {
+//    return (
+//      <div id='editor'></div>
+//    );
+//  }
+//});
+//
+//module.exports = EditorView;

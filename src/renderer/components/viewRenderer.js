0759ef75f858ad1c47a3ea83c777fa39cd161315
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2015-02-14 00:56:41 +0530

    Create component renderer entry point for node-jsx

---
+++ src/renderer/components/viewRenderer.js
@@ -0,0 +1,26 @@
+var React = require('react');
+
+var placeholder = 'Alas leggo my "leggo my eggo" legless lego legolas and leave illegible legalese legacies with legerity, lege of allegorist lasses';
+
+var SideMenu = require('./sideMenu');
+var UnderMenu = require('./underMenu');
+var NoteView = require('./noteView');
+
+module.exports = {
+  render: function() {
+    React.render(
+      <SideMenu />,
+      document.getElementById('sidemenu')
+    );
+
+    React.render(
+      <UnderMenu />,
+      document.getElementById('undermenu')
+    );
+
+    React.render(
+      <NoteView note={placeholder} />,
+      document.getElementById('noteview')
+    );
+  }
+};

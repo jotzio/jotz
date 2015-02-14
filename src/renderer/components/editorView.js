b0aaa53994ec4aa74f0d9c6c5efb506de4faedcd
minocys <harich.leung@gmail.com>
2015-02-14 10:25:22 -0500

    Begin adding language options

--- src/renderer/components/editorView.js
+++ src/renderer/components/editorView.js
@@ -1,22 +1,31 @@
 var React = require('react');
 var Ace = require('brace');
-require('brace/mode/javascript');
+require('./aceConfig');
 require('brace/theme/monokai');
 
+
 var EditorView = React.createClass({
+  changeLanguage: function (event) {
+    console.log(event.target.value);
+    this.editor.session.setMode('ace/mode/' + event.target.value);
+  },
   componentDidMount: function() {
-    var editor = Ace.edit('ace-editor');
-    editor.getSession().setMode('ace/mode/javascript');
-    editor.setOptions({
+    this.editor = Ace.edit('ace-editor');
+    this.editor.getSession().setMode('ace/mode/javascript');
+    this.editor.setOptions({
       maxLines: 400,
       minLines: 5
     });
-    editor.setTheme('ace/theme/monokai');
+    this.editor.setTheme('ace/theme/monokai');
   },
   render: function() {
     return (
       <div className='ace-editor-container'>
         <div id='ace-editor' className='ace-editor-inner'></div>
+        <select onChange={this.changeLanguage}>
+          <option value="javascript" selected>JavaScript</option>
+          <option value="coffeescript">CoffeeScript</option>
+        </select>
       </div>
     );
   }

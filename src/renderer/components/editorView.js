eaeefbbbe0acbe8b3fc265906d8c20b545d4de71
minocys <harich.leung@gmail.com>
2015-02-13 22:03:43 -0500

    Add ace editor component and scss

--- src/renderer/components/editorView.js
+++ src/renderer/components/editorView.js
@@ -5,13 +5,18 @@ require('brace/theme/monokai');
 
 var EditorView = React.createClass({
   componentDidMount: function() {
-    var editor = Ace.edit('editor');
+    var editor = Ace.edit('ace-editor');
     editor.getSession().setMode('ace/mode/javascript');
     editor.setTheme('ace/theme/monokai');
   },
   render: function() {
+    var style = {
+      height: '50vh',
+      width: '50vw',
+      backgroundColor: 'black'
+    };
     return (
-      <div id='editor'></div>
+      <div id='ace-editor' className='ace-editor-container'></div>
     );
   }
 });

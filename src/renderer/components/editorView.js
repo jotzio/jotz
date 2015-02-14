49647f52c57008278362680772758fcf08e8cac0
minocys <harich.leung@gmail.com>
2015-02-13 22:28:10 -0500

    Update ace editor styles

--- src/renderer/components/editorView.js
+++ src/renderer/components/editorView.js
@@ -7,16 +7,17 @@ var EditorView = React.createClass({
   componentDidMount: function() {
     var editor = Ace.edit('ace-editor');
     editor.getSession().setMode('ace/mode/javascript');
+    editor.setOptions({
+      maxLines: 400,
+      minLines: 5
+    });
     editor.setTheme('ace/theme/monokai');
   },
   render: function() {
-    var style = {
-      height: '50vh',
-      width: '50vw',
-      backgroundColor: 'black'
-    };
     return (
-      <div id='ace-editor' className='ace-editor-container'></div>
+      <div className='ace-editor-container'>
+        <div id='ace-editor' className='ace-editor-inner'></div>
+      </div>
     );
   }
 });

2bb747e19febe2ebf5323f0d0c867f7001b5556c
Clark Feusier <cfeusier@gmail.com>
2015-02-14 16:27:46 -0800

    Begin create-note handling in notebook store

--- src/renderer/stores/Notebook.js
+++ src/renderer/stores/Notebook.js
@@ -10,34 +10,34 @@ var Notebook = Backbone.Collection.extend({
   initialize: function() {
     this.dispatchToken = JotzDispatcher.register(this.dispatchCallback.bind(this));
     // listen for ipc completion event on client
-    // TODO: integrate with Clark
     // ipc.on('save-note-reply', function(arg) { console.log(arg + ' caught reply on renderer'); });
   },
 
   dispatchCallback: function(payload) {
     switch(payload.actionType) {
-      case 'create-note':
-        this.add({content: payload.content})
-        break;
       case 'new-note':
-        this.add({content: 'default note text'})
+        // TODO: display creation/editing view
+        break;
+      case 'create-note':
+        // add note to notebook store
+        var note = this.add({ content: payload.content });
+        this.saveNote(note);
         break;
       default:
         break;
     }
   },
 
-  // TODO: integrate with Clark
-  // saveNote: function(note) {
-  //
-  //   // fire off event with ipc
-  //   ipc.send('save-note-message', note);
-  //   // listen for ipc event on browser
-  //   // write to fs on broswer
-  //   // fire off completion event with ipc from browser
-  //   // collection save on client
-  //   // update react component on collection 'change' event (automatically)
-  // }
+  saveNote: function(note) {
+    console.log(note);
+    // fire off event with ipc
+    ipc.send('save-note-message', note);
+    // listen for ipc event on browser
+    // write to fs on broswer
+    // fire off completion event with ipc from browser
+    // collection save on client
+    // update react component on collection 'change' event (automatically)
+  }
 });
 
 var NotebookStore = new Notebook();

b06907e8ec47cac545388f9c49be8731f0d5eae3
Clark Feusier <cfeusier@gmail.com>
2015-02-16 17:21:56 -0800

    Finish basic notes api for saving and updating notes

--- src/browser/apis/notes_api.js
+++ src/browser/apis/notes_api.js
@@ -1,17 +1,34 @@
-var app = require('app');
 var fs = require('fs');
 var ipc = require('ipc');
 var utils = require('../utils/global');
 
 var NotesAPI = (function() {
-  return {
+
+  var api = {
     saveNote: function(note, cb) {
-      utils.getNotesDirData(function(noteFilenames) {
-        // look for filename based on note.title?
-        console.log(noteFilenames);
+      var filename = api.noteFilename(note.attributes._id, note.attributes.title);
+      api.findNote(filename, function() {
+          api.writeNote(filename, note.attributes, cb);
       });
+    },
+    findNote: function(filename, cb) {
+      utils.getNotesDirData(function(noteFilenames) {
+        noteFilenames.indexOf(filename) >= 0 ? cb(true) : cb(false);
+      });
+    },
+    noteFilename: function(noteId, noteTitle) {
+      return noteId + '_' + api.titleToFile(noteTitle) + '.json';
+    },
+    titleToFile: function(noteTitle) {
+      return noteTitle.toString().toLowerCase().replace(/\s+/gi, '_');
+    },
+    writeNote: function(filename, noteData, cb) {
+      var filePath = utils.getNotesDirPath() + filename;
+      fs.writeFile(filePath, JSON.stringify(noteData), { encoding: 'utf8' }, cb);
     }
   };
+
+  return api;
 })();
 
 module.exports = NotesAPI;

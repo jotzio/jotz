65cad3c79a1eb70911a5281db9bd8ce97144a598
Clark Feusier <cfeusier@gmail.com>
2015-02-16 17:23:02 -0800

    Cleanup global utils and add hashing function for ids

--- src/browser/utils/global.js
+++ src/browser/utils/global.js
@@ -2,7 +2,10 @@ var app = require('app');
 var fs = require('fs');
 
 var GlobalUtils = (function() {
+  // Private
+  var MAX_FOR_HASHING = 1000000;
 
+  // Public
   var api = {
     getAppDirPath: function() {
       return app.getDataPath();
@@ -10,51 +13,26 @@ var GlobalUtils = (function() {
     getNotesDirPath: function() {
       return api.getAppDirPath() + '/notes/';
     },
-    getNotesDirData: function(cb) {
-      fs.readdir(api.getNotesDirPath(), function(err, noteFilenames) {
-        if (err) {
-          api.createNotesDir(function() {
-            // TODO: how do we want to handle this UX?
-            console.log("no notes created yet");
-          });
-        } else {
-          cb(noteFilenames);
-        }
-      });
-    },
     createNotesDir: function(cb) {
       fs.mkdir(api.getNotesDirPath(), cb);
     },
-    getAppConfigPath: function() {
-      return api.getAppDirPath() + '/jotz_app_config.json';
-    },
-    getAppConfigData: function(cb) {
-      fs.readFile(api.getAppConfigPath(), 'utf8', function(err, fileData) {
-        err ? cb(err) : cb(null, fileData);
-      });
-    },
-    createAppConfigFile: function() {
-      api.getAppConfigData(function(err, fileData) {
-        if (err) {
-          // TODO: decide on standard app configuration properties
-          var appConfigs = { testConfig: false };
-          api.writeAppConfigFile(JSON.stringify(appConfigs));
+    getNotesDirData: function(cb) {
+      fs.readdir(api.getNotesDirPath(), function(err, noteFilenames) {
+        if (!err) {
+          cb(noteFilenames);
         } else {
-          return fileData;
+          api.createNotesDir(function() { cb([]) });
         }
       });
     },
-    writeAppConfigFile: function(configJSON) {
-      var opts = { encoding: 'utf8' };
-      fs.appendFile(api.getAppConfigPath(), configJSON, opts, function(err) {
-        if (err) {
-          // TODO: research error handling with Atom Shell
-        }
-        api.createAppConfigFile();
-      });
-    },
-    getUserConfigPath: function() {
-      return api.getAppDirPath() + '/jotz_user_config.json';
+    getIndexBelowMaxForKey: function(str) {
+      var hash = 0;
+      for (var i = 0; i < str.length; i++) {
+        hash = (hash<<5) + hash + str.charCodeAt(i);
+        hash = hash & hash;
+        hash = Math.abs(hash);
+      }
+      return hash % MAX_FOR_HASHING;
     }
   };
 

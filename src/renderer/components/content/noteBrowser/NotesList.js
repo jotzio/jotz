b047309d6be7e84e5e1ba4ef3dd5d41e19cc965e
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2015-02-20 00:16:49 +0530

    Add onClick event to noteList for view switching

--- src/renderer/components/content/noteBrowser/NotesList.js
+++ src/renderer/components/content/noteBrowser/NotesList.js
@@ -1,13 +1,21 @@
 var React = require('react');
+var _ = require('underscore');
 
 // TODO: Make each note clickable
 
 var NotesList = React.createClass({
 
+  handleClick: function(e) {
+    e.preventDefault();
+    var noteId = e.target.attributes.getNamedItem('data-note').value;
+    var note = this.props.allNotes.findWhere({ _id: noteId });
+    this.props.newNote(note.attributes);
+  },
+
   render: function() {
     var notes = this.props.allNotes.map(function(note) {
-      return <li key={note.get('_id')}>{note.get('title')}</li>;
-    });
+      return <li onClick={this.handleClick} data-note={note.get('_id')} key={note.get('_id')}>{note.get('title')}</li>;
+    }.bind(this));
     return (
       <div>
         <h1>Hooray for the NotesList!!!</h1>

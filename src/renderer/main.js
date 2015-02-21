e3f3c979937182f18b3d29f1f8ce2a7606f69998
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2015-02-22 00:06:27 +0530

    Add stores to renderer entry point as application-wide props

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,11 +1,14 @@
 var React = require('react');
-
-require('node-jsx').install();
+var Notes = require('./stores/notes');
+var Notebooks = require('./stores/notebooks');
 
 var Jotz = require('./components/jotz');
 
 module.exports = {
   init: function() {
-    React.render(<Jotz />, document.body);
+    React.render(<Jotz
+      notes={Notes}
+      notebooks={Notebooks}
+    />, document.body);
   }
 };

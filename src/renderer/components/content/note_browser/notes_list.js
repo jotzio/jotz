8be5d5e783093a2191bcfc951538469d27ea58f8
Omar Alvarez <omar.alvarez2006@gmail.com>
2015-02-23 03:26:05 +0000

    Splits note item and note list into separate components

--- src/renderer/components/content/note_browser/notes_list.js
+++ src/renderer/components/content/note_browser/notes_list.js
@@ -1,38 +1,17 @@
 var React = require('react');
-var _ = require('underscore');
+var NoteItem = require('./note_item');
 var actionCreator = require('../../../actions/action_creator');
-
-// TODO: Make each note clickable
 
 var NotesList = React.createClass({
 
-  handleClick: function(e) {
-    e.preventDefault();
-    var noteId = e.target.attributes.getNamedItem('data-note').value;
-    var note = this.props.notes.findWhere({ _id: noteId });
-    this.props.swapView('Editor', note);
-  },
-
-  handleDelete: function(e) {
-    e.preventDefault();
-    var noteId = e.target.attributes.getNamedItem('data-note').value;
-    var note = this.props.notes.findWhere({ _id: noteId });
-    actionCreator.destroyNote(note.attributes);
-  },
-
   render: function() {
-    var notes = this.props.notes.sortBy('_id').map(function(note) {
-      var noteId = note.get('_id');
-      return <li>
-              <a onClick={this.handleClick} data-note={noteId} key={noteId} href=''>{note.get('title')}</a>
-              <a onClick={this.handleDelete} data-note={noteId} href=''> delete</a>
-             </li>;
-    }.bind(this));
     return (
       <div>
         <h1>Hooray for the NotesList!!!</h1>
         <ul>
-          {notes}
+          {this.props.notes.map(function(note) {
+            return <NoteItem key={note.get('_id')} swapView={this.props.swapView} note={note} />;
+          }.bind(this))}
         </ul>
       </div>
     );

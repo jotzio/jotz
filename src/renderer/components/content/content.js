1d99e75ad8cbbd37bd514c0a33f8a2accbe05048
minocys <harich.leung@gmail.com>
2015-02-24 18:24:50 -0500

    Refactor view selection

--- src/renderer/components/content/content.js
+++ src/renderer/components/content/content.js
@@ -1,40 +1,41 @@
 var React = require('react');
 var Editor = require('./editor/editor');
 var NotesList = require('./note_browser/notes_list');
+var Notebooks = require('./note_browser/notebooks');
 var NotesStore = require('../../stores/notes');
+var NotebookStore = require('../../stores/notebooks');
 
 /*
   This manages what is displayed in the content area,
   Notes or Editor.
-  TODO: Create NoteBook View
  */
 
 var Content = React.createClass({
-  currentNote: void 0,
-
   changeNote: function(newView, note) {
     this.currentNote = note || void 0;
     this.props.swapView(newView);
   },
 
+  renderNotes: function() {
+    return <NotesList notes={NotesStore} swapView={this.props.swapView}/>;
+  },
+
+  renderNotebooks: function() {
+    return <Notebooks notes={NotesStore} noteBooks={NotebookStore} swapView={this.props.swapView}/>;
+  },
+
+  renderEditor: function() {
+    return <Editor note={this.props.currentNote} swapView={this.props.swapView}/>;
+  },
+
   //Checks view state, returns jsx for rendering
   renderContent: function() {
-    if (this.props.view === 'Notes') {
-      return (
-        <NotesList
-          filterQuery={this.props.filterQuery}
-          notes={NotesStore}
-          changeNote={this.changeNote}
-        />
-      );
-    } else if (this.props.view === 'Editor') {
-      return (
-        <Editor
-          note={this.currentNote}
-          changeNote={this.changeNote}
-        />
-      );
-    }
+    content = {
+      Notes: this.renderNotes,
+      Notebooks: this.renderNotebooks,
+      Editor: this.renderEditor
+    };
+    return content[this.props.view]();
   },
 
   render: function() {

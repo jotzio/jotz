c5be40170ff97aa979355aea92f6f208638ebbff
minocys <harich.leung@gmail.com>
2015-02-24 17:58:43 -0500

    Add notebooks skeleton

--- src/renderer/components/content/note_browser/notebooks.js
+++ src/renderer/components/content/note_browser/notebooks.js
@@ -1,1 +1,42 @@
 var React = require('react');
+var NoteItem = require('./note_item');
+
+var NoteBookList = React.createClass({
+
+  componentDidMount: function() {
+    this.props.notebooks.on('add remove reset', function() {
+      this.forceUpdate();
+    }.bind(this), this)
+  },
+
+  componentWillUnmount: function() {
+    this.props.notebooks.off(null, null, this);
+  },
+
+  renderNotes: function() {
+    return this.props.notes.map(function(note) {
+      return <NoteItem key={note.get('_id')} swapView={this.props.swapView} note={note} />;
+    }.bind(this));
+  },
+
+  renderNotebook: function() {
+    return this.props.notebooks.map(function(notebook, index) {
+      return (
+        <div>
+          <h1>Notebooks</h1>
+          <ul>
+            {this.renderNotes()}
+          </ul>
+        </div>
+      );
+    });
+  },
+
+  render: function() {
+    return (
+      <div>
+        {this.renderNoteBook()}
+      </div>
+    );
+  }
+});
